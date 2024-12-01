5054ebcf429cedb67b4434a6d2c192be26c46e8c
Eileen Xu <57423751+eileenxu9@users.noreply.github.com>
2024-12-01 13:18:53 -0500

    Update sig.py #3

--- sig.py
+++ sig.py
@@ -4,18 +4,20 @@ def generate_signature_keypair():
     sig = Signature("MAYO-3")
     public_key = sig.generate_keypair()
     private_key = sig.export_secret_key()
-    mayo3_sig.close()
+    sig.close()
     return public_key, private_key
+    
 #sign data using MAYO-3
 def sign_data(data, private_key):
     sig = Signature("MAYO-3")
     sig.import_secret_key(private_key)
     signature = sig.sign(data)
-    mayo3_sig.close()
+    sig.close()
     return signature
+    
 #verify a signature using MAYO-3
 def verify_signature(data, signature, public_key):
     sig = Signature("MAYO-3")
     is_valid = sig.verify(data, signature, public_key)
-    mayo3_sig.close()
+    sig.close()
     return is_valid
