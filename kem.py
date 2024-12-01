2c01c956e6df2b7b09bb018381fe86844e1467fd
ChrisCortesSanchez <79801197+ChrisCortesSanchez@users.noreply.github.com>
2024-12-01 14:23:47 -0500

    Added parameter to encap()
    Added parameter to encap() to incorporate symmetric key encryption
    
    Fixed issue in decap() function

--- kem.py
+++ kem.py
@@ -1,41 +1,35 @@
-import oqs
+from oqs import KeyEcapsulation
 
 # encapsulates key(server-side)
-# returns the public-key and private key used to encapsulate, ciphertext, and the shared secret
-def encap():
+# returns the public-key and private key used to encapsulate, ciphertext, the shared secret, and the client object 
+def encap(private_key):
     kemalg = "ML-KEM-768"
     # server's only purpose is to encapsulate the key
-    with oqs.KeyEncapsulation(kemalg) as server:
+    with KeyEncapsulation(kemalg, private_key) as server:
         
         # Client generates its key pair
-        with oqs.KeyEncapsulation(kemalg) as client:
-            public_key_client, private_key_client = client.generate_keypair()
+        with KeyEncapsulation(kemalg) as client:
+            public_key_client = client.generate_keypair()
+            private_key_client = client.export_secret_key()
 
         # Server encapsulates its secret using the client's public key
         ciphertext, server_shared_secret = server.encap_secret(public_key_client)
+        client_obj = client
         
-        return public_key_client, private_key_client, ciphertext, server_shared_secret
+        return public_key_client, private_key_client, ciphertext, server_shared_secret, client_obj
 
 
-# decapsulates key (client-side)
+# decapsulates key
+# client object is passed in so that the same private key can be used to decapsulate
 # returns the shared_secret obtained from the cipher text
-def decap(ciphertext, private_key_client):
-    kemalg = "ML-KEM-768"
-    with oqs.KeyEncapsulation(kemalg) as client:
-        # Use the existing private key for decapsulation
-        client.load_private_key(private_key_client)
-
-        # The client decapsulates the server's ciphertext using its private key
-        client_shared_secret = client.decap_secret(ciphertext)
-        
-        return client_shared_secret
+def decap(ciphertext, client_obj):
+    # The client decapsulates the server's ciphertext using its private key
+    client_shared_secret = client_obj.decap_secret(ciphertext)
+    return client_shared_secret
 
 
 # Example
-# public_key_client, private_key_client, ciphertext, server_shared_secret = encap()
-
-# # Decapsulation
-# client_shared_secret = decap(ciphertext, private_key_client)
-
+# public_key, private_key, ciphertext, server_shared_scecret, client_obj = encap()
+# client_shared_secret = decap(ciphertext, client_obj)
 # # Validate that both shared secrets match
 # print("\nShared secrets match:", server_shared_secret == client_shared_secret)
