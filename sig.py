d3bc2c8ca882e3c6587aa87121c99c5bc5dbc496
Eileen Xu <57423751+eileenxu9@users.noreply.github.com>
2024-12-01 14:38:59 -0500

    Update sig.py #4

--- sig.py
+++ sig.py
@@ -1,23 +1,23 @@
 from oqs import Signature
-#generate keypair for MAYO-3
+
+# generate keypair with MAYO-3
 def generate_signature_keypair():
     sig = Signature("MAYO-3")
     public_key = sig.generate_keypair()
     private_key = sig.export_secret_key()
-    sig.close()
+    sig.free()
     return public_key, private_key
-    
-#sign data using MAYO-3
+
+# sign data using the specified private key
 def sign_data(data, private_key):
-    sig = Signature("MAYO-3")
-    sig.import_secret_key(private_key)
-    signature = sig.sign(data)
-    sig.close()
+    sig = Signature("MAYO-3", secret_key=private_key)
+    signature = sig.sign(data.encode())
+    sig.free()
     return signature
-    
-#verify a signature using MAYO-3
+
+# verify the signature using the public key
 def verify_signature(data, signature, public_key):
     sig = Signature("MAYO-3")
-    is_valid = sig.verify(data, signature, public_key)
-    sig.close()
+    is_valid = sig.verify(data.encode(), signature, public_key)
+    sig.free()
     return is_valid
